187a99bee35bb0e0d01342fc32503c3dca80ad7a
Asterius27 <878352@stud.unive.it>
2022-05-16 16:01:32 +0200

    started socket io client

---
+++ battleships_frontend/src/app/socketio.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserHttpService } from './user-http.service';
+import io from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketioService {
+
+  private socket:any;
+  constructor(private us: UserHttpService) {}
+
+  connect(event:string) : Observable<any> {
+    this.socket = io(this.us.url);
+    return new Observable((observer) => {
+      this.socket.on(event, (arg:any) => { // TODO add handling of multiple args
+        console.log('Socket.io message received: ' + JSON.stringify(arg));
+        observer.next(arg);
+      });
+      this.socket.on('error', (err:any) => {
+        console.log('Socket.io error: ' + err);
+        observer.error(err);
+      });
+      const sck = this.socket; // TODO check if this works
+      return {
+        unsubscribe() {
+          sck.disconnect();
+        }
+      }
+    });
+  }
+}
