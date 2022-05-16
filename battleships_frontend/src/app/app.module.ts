805323bb6594c7224785f4d917bc3bb047414f05
Asterius27 <878352@stud.unive.it>
2022-05-16 16:01:16 +0200

    started socket io client

--- battleships_frontend/src/app/app.module.ts
+++ battleships_frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserHttpService } from './user-http.service';
 import { SignupComponent } from './signup/signup.component';
+import { SocketioService } from './socketio.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { SignupComponent } from './signup/signup.component';
     FormsModule
   ],
   providers: [
-    {provide: UserHttpService, useClass: UserHttpService}
+    {provide: UserHttpService, useClass: UserHttpService},
+    {provide: SocketioService, useClass: SocketioService}
   ],
   bootstrap: [AppComponent]
 })
