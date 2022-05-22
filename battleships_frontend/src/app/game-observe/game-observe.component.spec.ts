3b46f3707a7e51717809277bee8f7d4dec9e377c
Asterius27 <878352@stud.unive.it>
2022-05-22 12:48:51 +0200

    started working on observe game

---
+++ battleships_frontend/src/app/game-observe/game-observe.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GameObserveComponent } from './game-observe.component';
+
+describe('GameObserveComponent', () => {
+  let component: GameObserveComponent;
+  let fixture: ComponentFixture<GameObserveComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GameObserveComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GameObserveComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ battleships_frontend/src/app/game-observe/game-observe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-observe',
+  templateUrl: './game-observe.component.html',
+  styleUrls: ['./game-observe.component.css']
+})
+export class GameObserveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
